c6f1615b1153a6c5c4bc0cc56a5e320891edf925
Sujan Bhusal <sujan.bhusal@gmail.com>
2024-11-09 12:45:30 +0545

    adding calendar in principal

--- frontend/src/pages/calendar.jsx
+++ frontend/src/pages/calendar.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import MainLayout from '../layout/MainLayout'
+import { Calendar } from 'antd';
+import './css/calendar.css'
 
-const Calendar = () => {
+const AcademicCalendar   = () => {
+
+  const onPanelChange = (value, mode) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
+  }
+
   return (
     <MainLayout>
-    <div>Calendar</div>
+    <div className='flex flex-col item-center rounded-xl shadow-lg justify-center shadow-blue-900'> 
+    
+    <div className=' flex justify-center text-blue-600 text-2xl font-semibold '>Academic Calendar</div> 
+    <div className='pt-4 pl-5'> <Calendar onPanelChange={onPanelChange} /></div>
+     
+     </div>
     </MainLayout>
 
   )
 }
 
-export default Calendar
+export default AcademicCalendar
