dc3f35b15fb2ffeaafbd1fad6894e9d47940b908
aakash <aakash.singh.as140@gmail.com>
2024-11-17 13:38:47 +0545

    error correction

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -56,7 +56,7 @@ class LoginAPIView(APIView):
                     'date_of_birth': user.student.date_of_birth,
                     'gender': user.student.gender,
                     'parents': user.student.parents,
-                    'class': user.student.classes.name,
+                    'class': user.student.classes.class_name,
                 }
             else:
                 return Response({'error': 'User has no role assigned'}, status=status.HTTP_403_FORBIDDEN)
@@ -65,10 +65,6 @@ class LoginAPIView(APIView):
             response_data = {
                 'refresh': str(refresh),
                 'access': str(refresh.access_token),
-<<<<<<< HEAD
-                'role': 'master' if user.is_master else 'principal' if user.is_principal else 'teacher' if user.is_teacher else 'student' if user.is_student else 'staff' if user.is_staff else None,
-=======
->>>>>>> a58cfa2a20fee9bb05372540d922c0e75a1f6dc8
                 'username': username,
                 **role_data,  # Include role-specific data
                 'email': user.email,  # User's email (assuming it exists in your user model)
