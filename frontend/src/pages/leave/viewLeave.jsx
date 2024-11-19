e9896fd513ed91b569ed5708255866202a994ea1
Sujan Bhusal <sujan.bhusal@gmail.com>
2024-11-19 09:47:44 +0545

    view Leave functionality

--- frontend/src/pages/leave/viewLeave.jsx
+++ frontend/src/pages/leave/viewLeave.jsx
@@ -1,147 +1,121 @@
-import React, { useState } from 'react'
-import MainLayout from '../../layout/MainLayout'
-import {  Space, Table } from "antd";
-
-
+import React, { useEffect, useState } from "react";
+import MainLayout from "../../layout/MainLayout";
+import { Space, Table } from "antd";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const columns = [
-    {
-      title: "S.N.",
-      dataIndex: "key",
-      key: "key",
+  {
+    title: "ID",
+    dataIndex: "applicant",
+  },
+  {
+    title: "Applicant Name",
+    dataIndex: "applicant_name",
+  },
+  {
+    title: "Role",
+    dataIndex: "applicant_type",
+  },
+  {
+    title: "Class",
+    dataIndex: "class",
+  },
+  {
+    title: "Applied Date",
+    dataIndex: "applied_on",
+      },
+  {
+    title: "Leave Date",
+    dataIndex: "leave_date",
     },
-    {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
-    },
-    {
-      title: "Class",
-      dataIndex: "class",
-      key: "class",
-    },
-    {
-      title: "Date",
-      dataIndex: "date",
-      key: "date",
-    },
-    {
-      title: "Message",
-      key: "message",
-      dataIndex: "message",
-    },
-    {
-      title: "Action",
-      key: "action",
-      render: (_, record) => (
-        <Space size="middle">
-          <a>View </a>
-          <a>Accept</a>
-          <a>Reject</a>
-        </Space>
-      ),
-    },
-  ];
-
-
-  const data = [
-    {
-      key: "1",
-      name: "One",
-      class: "All Day",
-      date: "25",
-      message: "5",
-    },
-    {
-      key: "2",
-      name: "Two",
-      class: "All Day",
-      date: "55",
-      message: "5",
-    },
-    {
-      key: "3",
-      name: "Three",
-      class: "All Day",
-      date: "55",
-      message: "5",
-    },
-    {
-      key: "4",
-      name: "Four",
-      class: "All Day",
-      date: "55",
-      message: "5",
-    },
-    {
-      key: "5",
-      name: "Five",
-      class: "All Day",
-      date: "55",
-      message: "5",
-    },
-    {
-      key: "6",
-      name: "Six",
-      class: "All Day",
-      date: "55",
-      message: "5",
-    },
-    {
-      key: "7",
-      name: "Seven",
-      class: "All Day",
-      date: "55",
-      message: "5",
-    },
-  ];
-  
-
-
+  {
+    title: "Message",
+    dataIndex: "message",
+  },
+  {
+    title: "Action",
+    render: (_, record) => (
+      <Space size="middle">
+        <Link className="text-lg text-blue-500">View</Link>
+        <button   className="bg-green-700 text-white rounded-md shadow-md p-1 text-sm">
+          Accept
+        </button>
+        <button className="bg-red-700 text-white rounded-md shadow-md p-1 text-sm">
+          Reject
+        </button>
+      </Space>
+    ),
+  },
+  {
+    title: "status",
+    dataIndex: "status",
+  },
+];
 
 
 const ViewLeave = () => {
+  const [leaveData, setLeaveData] = useState([]);
+  const [leaveError, setLeaveError] = useState(null);
+  // const [leaveStatus, setLeaveStatus] = useState('pending')
 
-    const [leaveStatus, setLeaveStatus] = useState(null);
+  // const handeleLeaveStatus = (value) => {
+  //     setLeaveStatus(value);
+  //     console.log(value);
+  //   };
 
-    // const handeleLeaveStatus = (value) => {
-    //     setLeaveStatus(value);
-    //     console.log(value);
-    //   };
+  useEffect(() => {
+    async function getLeaveData() {
+      try {
+        const token = localStorage.getItem("token");
+        if (!token) {
+          setLeaveError("User is not authenticated. Please log in.");
+          return;
+        }
+
+        const { data } = await axios.get(
+          "http://localhost:8000/api/total-leave-applications/",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (data) {
+          setLeaveData(data);
+          console.log(data)
+        }
+      } catch (error) {
+        setLeaveError("Error fetching leave data");
+        console.log(error);
+      }
+    }
+    getLeaveData();
+  }, []);
+
 
   return (
     <MainLayout>
-       
-          <div className="flex flex-col items-center gap-2 w-full">
-            <h4 className="text-purple-800 font-semibold text-lg ">
-             Leave Request
-            </h4>
-          <Table className='w-full' columns={columns} dataSource={data} />
+      <div className="flex flex-col items-center gap-2 w-full">
+        <h4 className="text-purple-800 font-semibold text-lg ">
+          Leave Request
+        </h4>
+        {
+        leaveError ? (
+          <p>{leaveError}</p>
+        ):
+         leaveData.length > 0 ? (
+              <Table className="w-full"  columns={columns} dataSource={leaveData} />
+        ) : (
+          <p>No leave applications found.</p>
+        )
+      }
+
       </div>
     </MainLayout>
-   
-  )
-}
+  );
+};
 
-export default ViewLeave
-
-
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-      
-
+export default ViewLeave;
