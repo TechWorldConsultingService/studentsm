b89ee018b9ffc2d0877542b8ad3368369485a02a
Sujan Bhusal <sujan.bhusal@gmail.com>
2024-11-30 08:57:30 +0545

    managing redux for user login

--- frontend/src/redux/reducerSlices/studentSlice.jsx
+++ frontend/src/redux/reducerSlices/studentSlice.jsx
@@ -1,36 +1,49 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  refresh: "",
+  access: "",
+  role: "",
+  username: "",
+  phone: "",
+  address: "",
+  data_of_birth: "",
+  gender: "",
+  parents: "",
+  // class: {},
+  subjects: [],
+  email: "",
+  first_name: "",
+  last_name: "",
 
-const initialState = { 
-    user:{},
-    
-    phone: false,
-    address: '',
-    date_of_birth:'',
-    parents:'',
-    
-    // userDetails:{},
-}
+  // userDetails:{},
+};
 
 const userSlice = createSlice({
-  name: 'studentdetails',
+  name: "studentdetails",
   initialState: initialState,
 
   reducers: {
-    setStudentDetails(state,action){
-      const {user,phone,address,date_of_birth,parents} = action.payload
+    setStudentDetails(state, action) {
+      const { refresh,access,username,phone,address,data_of_birth,gender,parents, subjects,email,first_name,last_name } = action.payload;
       return {
-          ...state,
-          user: user,
-          phone:phone,
-          address:address,
-          date_of_birth:date_of_birth,
-          parents:parents
-        }
+        ...state,
+        refresh:refresh,
+        access:access,
+        username:username,
+        phone:phone,
+        address:address,
+        data_of_birth:data_of_birth,
+        gender:gender,
+        parents:parents,
+        subjects:subjects,
+        email:email,
+        first_name:first_name,
+        last_name:last_name
+      };
     },
   },
-})
+});
 
-
-export const{ setStudentDetails } = userSlice.actions
-export default userSlice.reducer
+export const { setStudentDetails } = userSlice.actions;
+export default userSlice.reducer;
