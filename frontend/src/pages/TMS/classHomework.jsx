0f6d1e93cf22c191d0cb4d7b1a163907f2fcfec9
Sujan Bhusal <sujan.bhusal@gmail.com>
2025-01-14 14:34:56 +0545

    assignment list by teacher

--- frontend/src/pages/TMS/classHomework.jsx
+++ frontend/src/pages/TMS/classHomework.jsx
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from "react";
 import NewAssignment from "./NewAssignment";
 import { useSelector } from "react-redux";
+import useFetchData from "../../hooks/useFetch";
+import { Link } from "react-router-dom";
+
 
 const ClassHomework = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); // State to control modal visibility
-
+  const [newHomeworkModal, setNewHomeworkModal] = useState(false);
+  const [previousAssignments, setPreviousAssignments] = useState([]);
+  const [runningAssignments, setRunningAssignments] = useState([]);
   const user = useSelector((state) => state.user);
   const { access } = user;
   const teacher_id = user?.id;
   const selectedClass = user?.selectedClass;
   const [subjects, setSubjects] = useState([]);
 
+  const todayDate = new Date().toISOString().split("T")[0];
+
+  const { fetchedData: homeworkList = [], loadingData, fetchData:fetchHomeworkList } = useFetchData(
+    "http://localhost:8000/api/teacher/assignments/"
+  );
+
   useEffect(() => {
-    if (access) {
+    if (access && teacher_id && selectedClass) {
       fetch(
         `http://localhost:8000/api/filter-subjects/?teacher=${teacher_id}&class_assigned=${selectedClass}`
       )
@@ -24,42 +34,38 @@ const ClassHomework = () => {
         })
         .catch((error) => {
           console.error("Error fetching subjects:", error);
-          setSubjects([]);
         });
     } else {
       console.error("Teacher ID or class is undefined");
     }
-  }, [access]);
+  }, [access, teacher_id, selectedClass]);
 
-  const previousAssignments = [
-    {
-      id: 1,
-      subject: "Math",
-      topics: "Algebra",
-      description: "Complete exercises 1-10",
-      dueDate: "2024-12-20",
-    },
-    {
-      id: 2,
-      subject: "History",
-      topics: "WWII",
-      description: "Read chapters 5-8",
-      dueDate: "2024-12-22",
-    },
-  ];
+  useEffect(() => {
+    if (homeworkList && todayDate) {
+      const running = []
+      const previous = []
 
-  const runningAssignments = [
-    {
-      id: 1,
-      subject: "Science",
-      topics: "Physics",
-      description: "Lab report on gravity",
-      dueDate: "2024-12-18",
-    },
-  ];
+      homeworkList.forEach((homework) => {
+        const dueDate = new Date(homework.due_date)
+        const today = new Date(todayDate)
 
-  // Toggle modal visibility
-  const toggleModal = () => setIsModalOpen(!isModalOpen);
+        if (dueDate >= today) {
+          running.push(homework);
+        } else {
+          previous.push(homework);
+        }
+      });
+
+      setRunningAssignments(running)
+      setPreviousAssignments(previous)
+    }
+}, [homeworkList, todayDate])
+
+  const toggleNewHomeworkModal = () => setNewHomeworkModal(!newHomeworkModal);
+
+  if (loadingData) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="bg-purple-50 p-6">
@@ -71,7 +77,7 @@ const ClassHomework = () => {
         {/* New Assignment Button */}
         <div className="mt-6 text-center">
           <button
-            onClick={toggleModal}
+            onClick={toggleNewHomeworkModal}
             className="bg-purple-700 text-white px-6 py-2 rounded-lg hover:bg-purple-800 focus:outline-none"
           >
             Create New Assignment
@@ -90,15 +96,34 @@ const ClassHomework = () => {
                   key={assignment.id}
                   className="p-4 border rounded-lg shadow-md hover:shadow-lg"
                 >
-                  <strong className="block text-purple-800">
+                  <span className="flex ">
+                    <strong className="block text-purple-800 mr-2">
+                      Subject:
+                    </strong>
                     {assignment.subject}
-                  </strong>
-                  <span className="text-gray-600">{assignment.topics}</span>
-                  <p className="italic text-gray-700 mt-2">
-                    {assignment.description}
-                  </p>
+                  </span>
+                  <span className="flex ">
+                    <strong className="block text-purple-800 mr-2">
+                      Topic:
+                    </strong>
+                    {assignment.assignment_name}
+                  </span>
+                  <span className="flex ">
+                    <strong className="block text-purple-800 mr-2">
+                      Description:
+                    </strong>
+                    <p className="italic"> {assignment.description}</p>
+                  </span>
                   <span className="text-gray-500 block mt-2">
-                    Due: {assignment.dueDate}
+                    Due: {assignment.due_date}
+                  </span>
+                  <span className="text-gray-500 block ">
+                    Assign Date: {" "}
+                    {
+                      new Date(assignment.assigned_on)
+                        .toISOString()
+                        .split("T")[0]
+                    }
                   </span>
                   <a
                     href={`/submissions/${assignment.id}`}
@@ -126,22 +151,41 @@ const ClassHomework = () => {
                   key={assignment.id}
                   className="p-4 border rounded-lg shadow-md hover:shadow-lg"
                 >
-                  <strong className="block text-purple-800">
+                  <span className="flex ">
+                    <strong className="block text-purple-800 mr-2">
+                      Subject:
+                    </strong>
                     {assignment.subject}
-                  </strong>
-                  <span className="text-gray-600">{assignment.topics}</span>
-                  <p className="italic text-gray-700 mt-2">
-                    {assignment.description}
-                  </p>
-                  <span className="text-gray-500 block mt-2">
-                    Due: {assignment.dueDate}
                   </span>
-                  <a
+                  <span className="flex ">
+                    <strong className="block text-purple-800 mr-2">
+                      Topic:
+                    </strong>
+                    {assignment.assignment_name}
+                  </span>
+                  <span className="flex ">
+                    <strong className="block text-purple-800 mr-2">
+                      Description:
+                    </strong>
+                    <p className="italic"> {assignment.description}</p>
+                  </span>
+                  <span className="text-gray-500 block mt-2">
+                    Due: {assignment.due_date}
+                  </span>
+                  <span className="text-gray-500 block ">
+                    Assign Date:{" "}
+                    {
+                      new Date(assignment.assigned_on)
+                        .toISOString()
+                        .split("T")[0]
+                    }
+                  </span>
+                  <Link
                     href={`/submissions/${assignment.id}`}
                     className="text-purple-700 hover:underline mt-2 block"
                   >
                     View Submissions
-                  </a>
+                  </Link>
                 </li>
               ))}
             </ul>
@@ -150,22 +194,23 @@ const ClassHomework = () => {
           )}
         </div>
 
-        {/* Modal */}
-        {isModalOpen && (
+        {/* New Homework Modal */}
+        {newHomeworkModal && (
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center z-50">
             <div className="bg-white p-6 rounded-lg shadow-lg w-1/2 max-h-[90vh] overflow-y-auto relative">
               <button
-                onClick={toggleModal}
-                className="absolute top-2 right-2 text-purple-700 hover:text-purple-900"
+                onClick={toggleNewHomeworkModal}
+                className="absolute top-2 text-2xl right-2 text-purple-700 hover:text-purple-900"
               >
                 X
               </button>
 
               <NewAssignment
-                closeModal={toggleModal}
+                closeModal={toggleNewHomeworkModal}
                 access={access}
                 subjects={subjects}
                 selectedClass={selectedClass}
+                fetchHomeworkList={fetchHomeworkList}
               />
             </div>
           </div>
