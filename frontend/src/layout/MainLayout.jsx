152f70da635cc5ac419e02ec70ab5dad526b5186
bipin <24479632+bipinstha7@users.noreply.github.com>
2025-03-16 14:57:22 +0545

    Fix: Scroll body content and sidebar independently

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import Sidebar from '../components/sidebar'
-import NavBar from '../components/navBar'
+import React from "react";
+import Sidebar from "../components/sidebar";
+import NavBar from "../components/navBar";
 
-const MainLayout = ({children}) => {
+const MainLayout = ({ children, childrenClass = "" }) => {
   return (
     <div>
-      <div className="flex h-full w-screen">
-      <Sidebar />
-      <div className=" flex w-full flex-col">
-        <NavBar />
-        <div className='bg-gray-100 p-4'>{children}</div>
+      <div className="flex h-screen w-screen">
+        <div className="w-64 h-full overflow-y-auto bg-gray-800 text-white">
+          <Sidebar />
+        </div>
+        <div className="flex w-full flex-col h-full flex-1">
+          <NavBar />
+          <div className="bg-gray-100 p-4 flex-1 overflow-y-auto">
+            {children}
+          </div>
+        </div>
       </div>
     </div>
-      </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
